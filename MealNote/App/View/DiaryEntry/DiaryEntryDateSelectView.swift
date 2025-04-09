5a63a70f5c3c4f4bf088583dfdf1e032231e257f
shimada mio <auxpin0811shimada@gmail.com>
2025-04-09 09:12:00 +0900

    rename file

---
+++ MealNote/App/View/DiaryEntry/DiaryEntryDateSelectView.swift
@@ -0,0 +1,45 @@
+//
+//  DiaryEntryDateSelectView.swift
+//  MealNote
+//
+//  Created by mio on 2025/04/06.
+//
+
+import SwiftUI
+
+struct DiaryEntryDateSelectView: View {
+    @Binding private var selectedDate: Date
+    @Binding private var isShowDatePicker: Bool
+    
+    init(selectedDate: Binding<Date>, isShowDatePicker: Binding<Bool>) {
+        self._selectedDate = selectedDate
+        self._isShowDatePicker = isShowDatePicker
+    }
+
+    var body: some View {
+        VStack {
+            HStack(spacing: 4) {
+                Text("日時を選択")
+                    .font(.system(size: 16))
+                Spacer()
+                TextButton(title: selectedDate.toString(.dateTimeJp)) {
+                    isShowDatePicker.toggle()
+                }
+            }
+
+            if isShowDatePicker {
+                DatePicker(
+                    "",
+                    selection: $selectedDate,
+                    displayedComponents: [.date, .hourAndMinute]
+                )
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .labelsHidden()
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                .shadow(radius: 5)
+            }
+        }
+    }
+}
